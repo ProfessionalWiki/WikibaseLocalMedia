a4abc50e105bfd613677ca3a9bb751dcf17e40cd
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-11 16:57:52 -0700

    Update installMediaWiki.sh

--- .github/workflows/installMediaWiki.sh
+++ .github/workflows/installMediaWiki.sh
@@ -23,13 +23,14 @@ echo '$wgDeprecationReleaseLimit = "1.33";' >> LocalSettings.php
 
 echo '$wgEnableWikibaseRepo = true;' >> LocalSettings.php
 echo '$wgEnableWikibaseClient = false;' >> LocalSettings.php
-echo 'require_once __DIR__ . "/extensions/Wikibase/repo/ExampleSettings.php";' >> LocalSettings.php
 
 if [ "$MW_BRANCH" == "REL1_34" ] || [ "$MW_BRANCH" == "REL1_35" ]; then
   echo 'require_once __DIR__ . "/extensions/Wikibase/repo/Wikibase.php";' >> LocalSettings.php
 else
   echo 'wfLoadExtension( "WikibaseRepository", __DIR__ . "/extensions/Wikibase/extension-repo.json" );' >> LocalSettings.php
 fi
+
+echo 'require_once __DIR__ . "/extensions/Wikibase/repo/ExampleSettings.php";' >> LocalSettings.php
 
 echo 'wfLoadExtension( "'$EXTENSION_NAME'" );' >> LocalSettings.php
 
